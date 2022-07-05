b5471188697780dc3ab085366637b10b05f57ca9
dsf <thudsf19@gmail.com>
2022-07-05 17:46:03 +0800

    add src2 for mips system instr

--- src/isa/mips32/instr/system.h
+++ src/isa/mips32/instr/system.h
@@ -5,26 +5,26 @@ def_EHelper(syscall) {
 }
 
 def_EHelper(eret) {
-  rtl_hostcall(s, HOSTCALL_PRIV, s0, NULL, PRIV_ERET);
+  rtl_hostcall(s, HOSTCALL_PRIV, s0, NULL, NULL, PRIV_ERET);
   rtl_jr(s, s0);
 }
 
 def_EHelper(mfc0) {
-  rtl_hostcall(s, HOSTCALL_CSR, dsrc2, NULL, id_dest->imm);
+  rtl_hostcall(s, HOSTCALL_CSR, dsrc2, NULL, NULL, id_dest->imm);
 }
 
 def_EHelper(mtc0) {
-  rtl_hostcall(s, HOSTCALL_CSR, NULL, dsrc2, id_dest->imm);
+  rtl_hostcall(s, HOSTCALL_CSR, NULL, dsrc2, NULL, id_dest->imm);
 }
 
 def_EHelper(tlbwr) {
-  rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, PRIV_TLBWR);
+  rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, NULL, PRIV_TLBWR);
 }
 
 def_EHelper(tlbwi) {
-  rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, PRIV_TLBWI);
+  rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, NULL, PRIV_TLBWI);
 }
 
 def_EHelper(tlbp) {
-  rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, PRIV_TLBP);
+  rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, NULL, PRIV_TLBP);
 }
