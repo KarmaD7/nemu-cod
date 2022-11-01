5eedaa83974ba631f5de03b11da3e0de9f16ced8
dsf <thudsf19@gmail.com>
2022-11-01 11:15:43 +0800

    run basic monitor

--- src/isa/riscv32/init.c
+++ src/isa/riscv32/init.c
@@ -11,15 +11,34 @@ static const uint32_t img [] = {
   0x0000006b,  // nemu_trap
 };
 
+void init_csr();
+#ifndef CONFIG_SHARE
+void init_clint();
+#endif
+void init_device();
+
+
 static void restart() {
+  init_csr();
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
 
   /* The zero register is always 0. */
+  cpu.mode = MODE_M;
   cpu.gpr[0]._32 = 0;
 #ifndef __ICS_EXPORT
-  sstatus->val = 0x000c0100;
+  // sstatus->val = 0x000c0100;
+  // mstatus->val = 0x000c0100;
+  sstatus->val = 0;
+  mstatus->val = 0;
+  pmpcfg0->val = 0;
+  pmpcfg1->val = 0;
+  pmpcfg2->val = 0;
+  pmpcfg3->val = 0;
 #endif
+
+  IFNDEF(CONFIG_SHARE, init_clint());
+  IFDEF(CONFIG_SHARE, init_device());
 }
 
 void init_isa() {
